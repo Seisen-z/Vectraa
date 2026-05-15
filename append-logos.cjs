444673e6c2158ec3aaa2bb806878cb5a4b442736
Seisen88 <zandrixjanpongos46@gmail.com>
2026-05-16 00:53:38 +0800

    chore: remove scratch files

--- append-logos.cjs
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('scripts/custom-icons.ts', 'utf8');
-const newIcons = fs.readFileSync('logo-icons.txt', 'utf8');
-content = content.replace(/\s*\];\s*$/, '},\n' + newIcons + '\n];\n');
-fs.writeFileSync('scripts/custom-icons.ts', content);
-console.log('Appended logo icons successfully');
